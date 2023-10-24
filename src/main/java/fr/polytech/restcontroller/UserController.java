508f6f76df32efae0d9d5e6171619416488314b2
romainfrezier <romainfrezier@gmail.com>
2023-10-24 15:17:47 +0200

    refactor(user service): Move code from controller to service. Add comments.

--- src/main/java/fr/polytech/restcontroller/UserController.java
+++ src/main/java/fr/polytech/restcontroller/UserController.java
@@ -4,21 +4,17 @@ import fr.polytech.model.KeycloakLoginResponse;
 import fr.polytech.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.*;
-import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.HttpClientErrorException;
-import org.springframework.web.client.RestTemplate;
 
-import java.util.Map;
 
 @RestController
 @RequestMapping("/api/v1/user")
 public class UserController {
 
+    @Autowired
     private final UserService userService;
-
-    private final RestTemplate restTemplate = new RestTemplate();
 
     /**
      * Constructor for UserController
@@ -36,14 +32,19 @@ public class UserController {
      */
     @PostMapping("/auth/login")
     public ResponseEntity<KeycloakLoginResponse> login(@RequestBody MultiValueMap<String, String> formParams) {
-        String url = System.getenv("LOGIN_URI");
-
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
-
-        HttpEntity<MultiValueMap<String, String>> request = new HttpEntity<>(formParams, headers);
-
-        return restTemplate.exchange(url, HttpMethod.POST, request, KeycloakLoginResponse.class);
+        try {
+            return new ResponseEntity<>(userService.loginUser(formParams), HttpStatus.OK);
+        } catch (HttpClientErrorException e) {
+            if (e.getStatusCode() == HttpStatus.UNAUTHORIZED) {
+                return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+            } else if (e.getStatusCode() == HttpStatus.FORBIDDEN) {
+                return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
+            } else if (e.getStatusCode() == HttpStatus.BAD_REQUEST) {
+                return ResponseEntity.badRequest().build();
+            } else {
+                return ResponseEntity.internalServerError().build();
+            }
+        }
     }
 
     /**
@@ -53,60 +54,12 @@ public class UserController {
      */
     @PostMapping("/auth/register")
     public ResponseEntity<String> register(@RequestBody String jsonContent) {
-        String adminTokenUri = System.getenv("ADMIN_TOKEN_URI");
-        String registerUri = System.getenv("REGISTER_URI");
-
-        // Get admin token
-        // Post request with x-www-form-urlencoded content
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
-
-        MultiValueMap<String, String> map = new LinkedMultiValueMap<>();
-        map.add("grant_type", "password");
-        map.add("client_id", "admin-cli");
-        map.add("username", System.getenv("ADMIN_USERNAME"));
-        map.add("password", System.getenv("ADMIN_PASSWORD"));
-
-        HttpEntity<MultiValueMap<String, String>> request = new HttpEntity<>(map, headers);
-
-        ResponseEntity<Map> response = restTemplate.postForEntity(adminTokenUri, request, Map.class);
-
-        // Get access token from response
-        String accessToken;
-        try { // The get method can throw a NullPointerException
-            accessToken = (String) response.getBody().get("access_token");
-        } catch (NullPointerException e) {
-            return new ResponseEntity<>("An error occurred on the authentication server", HttpStatus.INTERNAL_SERVER_ERROR);
-        }
-
-        // Register user
-        // Post request with access token in Authorization header and jsonContent in body
-        HttpHeaders registerHeaders = new HttpHeaders();
-        registerHeaders.setContentType(MediaType.APPLICATION_JSON);
-        registerHeaders.setBearerAuth(accessToken);
-
-        HttpEntity<String> registerRequest = new HttpEntity<>(jsonContent, registerHeaders);
-
         try {
-            ResponseEntity<String> registerResponse = restTemplate.postForEntity(registerUri, registerRequest, String.class);
-            return new ResponseEntity<>(registerResponse.getBody(), registerResponse.getStatusCode());
+            return new ResponseEntity<>(userService.registerUser(jsonContent), HttpStatus.CREATED);
         } catch (HttpClientErrorException e) {
-            if (e.getStatusCode() == HttpStatus.CONFLICT) {
-                return new ResponseEntity<>("User exists with the same email", HttpStatus.CONFLICT);
-            }
-            if (e.getStatusCode() == HttpStatus.BAD_REQUEST) {
-                return new ResponseEntity<>("Invalid request", HttpStatus.BAD_REQUEST);
-            }
-            if (e.getStatusCode() == HttpStatus.UNAUTHORIZED) {
-                return new ResponseEntity<>("Unauthorized", HttpStatus.UNAUTHORIZED);
-            }
-            if (e.getStatusCode() == HttpStatus.FORBIDDEN) {
-                return new ResponseEntity<>("Forbidden", HttpStatus.FORBIDDEN);
-            }
-            if (e.getStatusCode() == HttpStatus.INTERNAL_SERVER_ERROR) {
-                return new ResponseEntity<>("An error occurred on the authentication server", HttpStatus.INTERNAL_SERVER_ERROR);
-            }
             return new ResponseEntity<>(e.getMessage(), e.getStatusCode());
+        } catch (Exception e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 }
--- src/main/java/fr/polytech/service/UserService.java
+++ src/main/java/fr/polytech/service/UserService.java
@@ -1,7 +1,118 @@
 package fr.polytech.service;
 
+import fr.polytech.model.KeycloakLoginResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.*;
 import org.springframework.stereotype.Service;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.RestTemplate;
+
 
 @Service
 public class UserService {
+
+    private final Logger logger = LoggerFactory.getLogger(UserService.class);
+    private final RestTemplate restTemplate = new RestTemplate();
+
+    /**
+     * Login user
+     * @param formParams Form parameters (x-www-form-urlencoded)
+     * @return KeycloakLoginResponse containing the response from the API
+     * @throws HttpClientErrorException if the API returns an error
+     */
+    public KeycloakLoginResponse loginUser(MultiValueMap<String, String> formParams) throws HttpClientErrorException {
+        logger.info("Starting the login process");
+        String url = System.getenv("LOGIN_URI");
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+
+        HttpEntity<MultiValueMap<String, String>> request = new HttpEntity<>(formParams, headers);
+
+        try {
+            ResponseEntity<KeycloakLoginResponse> response = restTemplate.postForEntity(url, request, KeycloakLoginResponse.class);
+            logger.info("User login completed");
+            return response.getBody();
+        } catch (HttpClientErrorException e) {
+            logger.error("Error while logging in user: " + e.getMessage());
+            throw new HttpClientErrorException(e.getStatusCode());
+        }
+    }
+
+    /**
+     * Register user
+     * @param jsonContent JSON content
+     * @return String containing the response from the API
+     * @throws HttpClientErrorException if the API returns an error
+     * @throws Exception if the admin access token cannot be retrieved
+     */
+    public String registerUser(String jsonContent) throws Exception {
+        logger.info("Starting the registration process");
+        String adminAccessToken = getAdminAccessToken();
+        if (adminAccessToken == null) {
+            logger.error("Failed to get admin access token");
+            throw new Exception("Failed to get admin access token");
+        }
+        String response = registerWithAccessToken(jsonContent, adminAccessToken);
+        logger.info("User registration completed");
+        return response;
+    }
+
+    /**
+     * Get the admin access token
+     * @return String containing the admin access token
+     * @throws HttpClientErrorException if the API returns an error
+     */
+    private String getAdminAccessToken() throws HttpClientErrorException {
+        String adminTokenUri = System.getenv("ADMIN_TOKEN_URI");
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+
+        MultiValueMap<String, String> map = new LinkedMultiValueMap<>();
+        map.add("grant_type", "password");
+        map.add("client_id", "admin-cli");
+        map.add("username", System.getenv("ADMIN_USERNAME"));
+        map.add("password", System.getenv("ADMIN_PASSWORD"));
+
+        HttpEntity<MultiValueMap<String, String>> request = new HttpEntity<>(map, headers);
+
+        ResponseEntity<KeycloakLoginResponse> response;
+        try {
+            response = restTemplate.postForEntity(adminTokenUri, request, KeycloakLoginResponse.class);
+            return response.getBody().getAccess_token();
+        } catch (HttpClientErrorException e) {
+            logger.error("Error while getting admin access token: " + e.getMessage());
+            throw new HttpClientErrorException(e.getStatusCode());
+        } catch (NullPointerException e) {
+            logger.error("Error while getting admin access token: " + e.getMessage());
+            throw new HttpClientErrorException(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    /**
+     * Register user with access token
+     * @param jsonContent JSON content
+     * @param accessToken Access token
+     * @return String containing the response from the API
+     * @throws HttpClientErrorException if the API returns an error
+     */
+    private String registerWithAccessToken(String jsonContent, String accessToken) throws HttpClientErrorException {
+        String registerUri = System.getenv("REGISTER_URI");
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        headers.setBearerAuth(accessToken);
+
+        HttpEntity<String> request = new HttpEntity<>(jsonContent, headers);
+
+        try {
+            ResponseEntity<String> response = restTemplate.postForEntity(registerUri, request, String.class);
+            return response.getBody();
+        } catch (HttpClientErrorException e) {
+            logger.error("Error while registering user: " + e.getMessage());
+            throw new HttpClientErrorException(e.getStatusCode());
+        }
+    }
 }
